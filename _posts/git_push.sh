c476729c0c05e20bd581bddaf13a8853c741eea0
ztb1211 <xiaocorn96@gmail.com>
2018-06-29 00:18:18 +0800

    add two algorithms

--- _posts/git_push.sh
+++ _posts/git_push.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 git add .
 echo "========================="
-echo "========git add .========"
+echo "==       git add .     =="
 echo "========================="
 git commit -m 'add two algorithms'
 echo "========================="
-echo "========git commit======="
+echo "==      git commit     =="
 echo "========================="
 git push
 echo "========================="
-echo "========git push========="
+echo "==      git push       =="
 echo "========================="

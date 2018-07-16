6fd7b48810f98c663ef6702c2b5bbc2ead1eda69
ztb1211 <xiaocorn96@gmail.com>
2018-07-17 01:17:47 +0800

    upload image

---
+++ _posts/image/git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git add .
+echo "========================="
+echo "==       git add .     =="
+echo "========================="
+git commit -m 'upload image'
+echo "========================="
+echo "==      git commit     =="
+echo "========================="
+git push
+echo "========================="
+echo "==      git push       =="
+echo "========================="

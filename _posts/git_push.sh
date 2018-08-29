afbc53d05309fb86edeb9f07dec1077f11834715
ztb1211 <xiaocorn96@gmail.com>
2018-08-29 23:58:00 +0800

    add linux collection

--- _posts/git_push.sh
+++ _posts/git_push.sh
@@ -3,7 +3,7 @@ git add .
 echo "========================="
 echo "==       git add .     =="
 echo "========================="
-git commit -m 'add JavaWeb'
+git commit -m 'add linux collection'
 echo "========================="
 echo "==      git commit     =="
 echo "========================="
